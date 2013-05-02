6b08e7b11cbcaf09d63d0264a61a368eeb31cddd
ebinion <ezekiel@gridshiftdesign.com>
2013-05-01 23:14:20 -0500

    Last header and footer changes before going live with the MVP

--- footer.php
+++ footer.php
@@ -5,15 +5,9 @@
         <div class="clearfix">
           <div class="eight columns">
             <p class="copyright">©<?php echo date("Y"); ?> Ezekiel C. Binion<br>
-              <a href="about.html">Chicago-based UX/UI designer, front end developer, Ruby on Rails engineer,</a> &amp; <a href="http://gridshiftdesign.com">solopreneur at Grid Shift LLC</a></p>
+              <a href="about.html">Chicago-based UX/UI designer, front end developer, &amp; Ruby on Rails engineer</p>
           </div>
-            <?php social_footer_menu(); ?>
-            <!-- <ul class="horizontal social">
-              <li><a href="http://github.com/ebinion" class="ss-icon ss-social-circle ss-octocat" title="Github"></a></li>
-              <li><a href="https://plus.google.com/114970225481656041603/" class="ss-icon ss-social-circle ss-googleplus" title="Github"></a></li>
-              <li><a href="http://www.linkedin.com/pub/ezekiel-binion/9/2b7/870" class="ss-icon ss-social-circle ss-linkedin" title="Linkedin"></a></li>
-              <li><a href="http://twitter.com/ebinion" class="ss-icon ss-social-circle ss-twitter" title="Twitter"></a></li>
-            </ul> -->
+          <?php social_footer_menu(); ?>
         </div>
       </div>
     </footer>
--- header.php
+++ header.php
@@ -18,12 +18,12 @@
   <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID, and uncomment below. -->
-    <!-- <script>
-      var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    <script>
+      var _gaq=[['_setAccount','UA-23433841-1'],['_trackPageview']];
       (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
       g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
       s.parentNode.insertBefore(g,s)}(document,'script'));
-    </script> -->
+    </script>
   <?php wp_head(); ?>
 </head>
 <body class="">
