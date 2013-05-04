474607f1eacfd3af44edbe1a69a743363b16afa2
ebinion <ezekiel@gridshiftdesign.com>
2013-05-04 11:53:41 -0500

    Fixed a bug that prevented me from displaying the social navigation appropiately

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
         <div class="clearfix">
           <div class="eight columns">
             <p class="copyright">©<?php echo date("Y"); ?> Ezekiel C. Binion<br>
-              <a href="about.html">Chicago-based UX/UI designer, front end developer, &amp; Ruby on Rails engineer</p>
+              <a href="about.html">Chicago-based UX/UI designer, front end developer, &amp; Ruby on Rails engineer</a></p>
           </div>
           <?php social_footer_menu(); ?>
         </div>
