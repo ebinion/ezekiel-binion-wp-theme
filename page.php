1d871134edc7cac0046170d61d3db207e0b557e7
ebinion <ezekiel@gridshiftdesign.com>
2013-06-05 00:20:08 -0500

    Recent Work part looks good.

---
+++ page.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying single pages
+ *
+ * @package WordPress
+ * @subpackage Ezekiel
+ * @since Ezekiel 1.0
+*/ 
+?>
+<?php get_template_part("header"); ?>
+<div class="container">
+  <div class="clearfix">
+    <div class="span12 columns">
+      <h2 class="sectionTitle">Case Study</h2>
+    </div>
+  </div>
+  <div class="row">
+    <div class="span4 columns portfolioLead">
+      <span class="category tags">Expert Analysis / Interation Design</span>
+      <h1>Improving Craigslist</h1>
+      <p>Here's a brief but awesome description of the project.</p>
+    </div>
+    <div class="span6">
+
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <h2 class="rowless sectionTitle">Recent Work</h2>
+    <ul class="portfolioSample list">
+        <li>
+          <a href="#">
+            <div class="projectImage"></div>
+            <h3 class="projectTitle">Title of Project for Company Name </h3>
+            <p class="category tags">Design in the Browser / Rapid Prototyping / Buckets of Farts</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="projectImage"></div>
+            <h3 class="projectTitle">Title of Project for Company Name </h3>
+            <p class="category tags">Design in the Browser / Rapid Prototyping / Buckets of Farts</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="projectImage"></div>
+            <h3 class="projectTitle">Title of Project for Company Name </h3>
+            <p class="category tags">Design in the Browser / Rapid Prototyping / Buckets of Farts</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="projectImage"></div>
+            <h3 class="projectTitle">Title of Project for Company Name </h3>
+            <p class="category tags">Design in the Browser / Rapid Prototyping / Buckets of Farts</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="projectImage"></div>
+            <h3 class="projectTitle">Title of Project for Company Name </h3>
+            <p class="category tags">Design in the Browser / Rapid Prototyping / Buckets of Farts</p>
+          </a>
+        </li>
+      </ul>
+  </div>
+</div>
+
+<?php get_template_part("footer"); ?>
